5f65bc22710a42481e7996b198e2a4c5fa6d6bdd
rakibahmeddev <rakibahmed.dev@gmail.com>
2025-06-25 22:00:12 +0600

    used a hook called useRouteError() to display 404 page if page not exist

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,22 @@
+import { Link, useRouteError } from "react-router-dom";
 
 
 
 const ErrorPage = () => {
+    const error = useRouteError();
+    console.log(error)
     
     return (
-        <div className="flex text-center justify-center pt-36">
+        <div className="flex flex-col text-center justify-center pt-36">
             <h2 className="text-5xl">Oops!!! 404 Page Not Found !!!</h2>
+
+            <div className="flex flex-col mt-6">
+                <p>{error.statusText}</p>
+           
+            <p>{error.error.message}</p>
+           
+            </div>
+             <Link to="/"><button className="bg-black text-white px-8 py-2 rounded-md mt-6">Home</button></Link>
             
         </div>
     );
